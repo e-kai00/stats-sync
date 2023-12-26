6f17d8c40bb8974f09195d0fec8042e5546946a4
Katerina <yelkaterina@gmail.com>
2023-12-26 21:19:27 +0100

    Add route to fetch Shopify order data and print order details

--- statsapp/routes.py
+++ statsapp/routes.py
@@ -4,15 +4,34 @@ from statsapp import app
 from statsapp import SHOPIFY_API_BASE_URL, SHOPIFY_HEADERS 
 
 
-@app.route('/get_shopify_data', methods=['GET'])
+@app.route('/get-shopify-data', methods=['GET'])
 def get_shopify_data():
 
-    shopify_response = requests.get(f"{SHOPIFY_API_BASE_URL}/orders/.json?status=any&fields=id", headers=SHOPIFY_HEADERS)
+    shopify_response = requests.get(
+        f"{SHOPIFY_API_BASE_URL}/orders.json?status=any&created_at_min=2023-09-31T15:57:11-04:00&fields=id,created_at,subtotal_price,total_price,total_tax,total_shipping_price_set,line_items", 
+        headers=SHOPIFY_HEADERS
+    )
 
-    return jsonify(shopify_response.json())
-    # return shopify_response.text
+    orders = shopify_response.json()
 
-    
+    for order in orders.get('orders', []):
+
+        order_id = order.get('id', None)
+        order_total = order.get('total_price', None)
+        order_subtotal = order.get('subtotal_price', None)
+        shipping = order.get('total_shipping_price_set', {}).get('shop_money', {}).get('amount', None)
+        
+        for line_item in order.get('line_items', []):
+            sku = line_item.get('sku', None)
+
+
+        print(
+            f'Order ID: {order_id}, Order total: {order_total}, Order subtotal: {order_subtotal}, Shipping: {shipping}, SKU: {sku}' 
+        )
+
+    # return jsonify(shopify_response.json())
+    return orders
+   
 
 
 

5a5ea97a998c59fd38f8d521c67d03fb36f73c56
Katerina <yelkaterina@gmail.com>
2024-01-15 17:38:13 +0100

    Style custom error pages

--- errors/handlers.py
+++ errors/handlers.py
@@ -2,14 +2,15 @@ from flask import flash, redirect, url_for, render_template
 
 
 def handle_bad_request(e):
-    # flash('An unexpected error occurred. Please try again later.', 'error')
     print(f'error happened {str(e)}')
     return render_template('error/400.html'), 400
 
 def page_not_found(e):
-  return render_template('error/404.html'), 404
+    print(f'error happened {str(e)}')
+    return render_template('error/404.html'), 404
 
-def internal_server_error(e):    
+def internal_server_error(e):
+    print(f'error happened {str(e)}')
     return render_template('error/500.html'), 500
 
 def handle_sqlalchemy_error(e, redirect_to='spend_snap'):
